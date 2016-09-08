4783e057e79ea13e5fa8d9644606f85408f56144
zhenxq <997382956@qq.com>
2016-09-09 00:18:06 +0800

    compatible python2 and python Get url baidu long/short sina short

--- short_url.py
+++ short_url.py
@@ -1,8 +1,15 @@
 #coding:utf-8
-import urllib2
-import urllib
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+try:
+    from urllib.request import Request
+    from urllib.request import urlopen
+except:
+    from urllib2 import Request
+    from urllib2 import urlopen
 import json
-
 
 headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"}
 
@@ -17,15 +24,19 @@ class ChangeUrl(object):
         self.short_url = short_url
 
     def getUrlUseBaidu(self,request):
-        response = urllib2.urlopen(request)
-        response_dict = json.loads(response.read())
-        if response_dict["status"] != 0:
-            return "err_msg", response_dict["err_msg"]
-        else:
-            if response_dict.has_key("tinyurl"):
-                return response_dict["tinyurl"]
+        try:
+            response = urlopen(request,timeout=2)
+            response_dict = json.loads(response.read().decode("utf-8"))
+            if response_dict["status"] != 0:
+                return "err_msg", response_dict["err_msg"]
             else:
-                return response_dict["longurl"]
+                if "tinyurl" in response_dict:
+                    return response_dict["tinyurl"]
+                else:
+                    return response_dict["longurl"]
+        except Exception as e:
+            print()
+            return None
 
     def baiduUrlLongToShort(self,long_url=0):
         """
@@ -33,8 +44,8 @@ class ChangeUrl(object):
         """
         if long_url:
             self.long_url = long_url
-        data = urllib.urlencode({'url': self.long_url})
-        request = urllib2.Request("http://dwz.cn/create.php", data=data, headers=headers)
+        data = urlencode({'url': self.long_url}).encode("utf-8")
+        request = Request("http://dwz.cn/create.php", data=data, headers=headers)
         return self.getUrlUseBaidu(request)
 
     def baiduUrlShortToLong(self,short_url=0):
@@ -43,8 +54,8 @@ class ChangeUrl(object):
         """
         if short_url:
             self.short_url = short_url
-        data = urllib.urlencode({'tinyurl': self.short_url})
-        request = urllib2.Request("http://dwz.cn/query.php", data=data, headers=headers)
+        data = urlencode({'tinyurl': self.short_url}).encode("utf-8")
+        request = Request("http://dwz.cn/query.php", data=data, headers=headers)
         return self.getUrlUseBaidu(request)
 
     def sinaUrlLongToShort(self,long_url=0):
@@ -56,17 +67,17 @@ class ChangeUrl(object):
         # source = ("3213676317", "3271760578")
         try:
             url = "http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long=" + self.long_url
-            request = urllib2.Request(url)
-            response = urllib2.urlopen(request,timeout=2)
-            response_dict = json.loads(response.read())[0]
+            request = Request(url)
+            response = urlopen(request,timeout=2)
+            response_dict = json.loads(response.read().decode("utf-8"))[0]
             return response_dict["url_short"]
         except Exception as e:
-            print e
+            print(e)
 
 if __name__ == "__main__":
     change_url = ChangeUrl()
     short_url = change_url.baiduUrlLongToShort("https://www.baidu.com/cache/sethelp/help.html")
     long_url =  change_url.baiduUrlShortToLong("http://dwz.cn/2btGVg")
-    print short_url,long_url
+    print(short_url,long_url)
     short_url = change_url.sinaUrlLongToShort("https://www.baidu.com/cache/sethelp/help.html")
-    print short_url
+    print(short_url)
